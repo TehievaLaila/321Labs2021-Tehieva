be2a46aeac904077bc8f5f7166d27be4e74fa4f7
Техиева Лайла Исламовна <211920@mck-ktits.ru>
2021-09-07 13:34:55 +0300

    Добавьте файлы проекта.

---
+++ Group321/Operations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Operations
+    {
+        public static void ShowBalance(Account account)
+        {
+            System.Console.WriteLine($"YOUR BALANCE: {account.Ballance}");
+        }
+
+        public static void Transaction(Account accountSeller, Account accountGetter, double summ)
+        {
+            accountSeller.Ballance -= summ;
+            accountGetter.Ballance += summ;
+            System.Console.WriteLine("TRANSACTION COMPLETED");
+            Console.WriteLine($"FIRST ACCOUNT : {accountSeller.Ballance} SECOND ACCOUNT: {accountGetter.Ballance}");
+        }
+
+        public static void Widtraw(Account accountSeller, double summ)
+        {
+            if (accountSeller.Ballance >= summ)
+            {
+                accountSeller.Ballance -= summ;
+                System.Console.WriteLine("TRANSACTION COMPLETED");
+                Console.WriteLine($"YOUR ACCOUNT HAS BEEN CHARGED: {summ} YOUR BALANCE: {accountSeller.Ballance}");
+            }
+            else
+            {
+                Console.WriteLine("THIS OPERATION IS IMPOSSIBLE!");
+            }
+
+
+        }
+
+        public static void Deposit(Account acoountGetter, double summ)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Group321/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            Account acc2 = new Account();
+            acc.Ballance = 5000;
+            acc2.Ballance = 8000;
+            Operations.ShowBalance(acc);
+            Operations.Transaction(acc, acc2, 500.00);
+            Operations.Widtraw(acc, 500.00);
+        }
+    }
+}
